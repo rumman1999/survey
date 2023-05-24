8169d411b7fa52c5835540975894b4ce8863b3e0
Ankita Tiwari <ankitatiwari12001@gmail.com>
2023-05-24 15:05:30 +0530

    css updated

--- src/components/SignIn/Register.js
+++ src/components/SignIn/Register.js
@@ -3,11 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import "./Register.css";
 import { BrowserRouter as Router, Link } from 'react-router-dom';
-const REACT_APP_API_ENDPOINT='http://localhost:5001'
+const REACT_APP_API_ENDPOINT = 'http://localhost:5001'
 
 
 function Register() {
-     const navigate = useNavigate();
+    const navigate = useNavigate();
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
@@ -70,98 +70,99 @@ function Register() {
                 body: JSON.stringify(registrationData)
             })
                 .then(response => {
-                        if (response.status === 201) {
-                            console.log('Registration successful');
-                            navigate('/');
-                        } else {
-                            if (response.status === 409) {
-                                console.log('Registration unsuccessful')
-                                setFail("User with this email already exists plss login")
-                            }
-                            throw new Error(response);
-                        }})
+                    if (response.status === 201) {
+                        console.log('Registration successful');
+                        navigate('/');
+                    } else {
+                        if (response.status === 409) {
+                            console.log('Registration unsuccessful')
+                            setFail("User with this email already exists plss login")
+                        }
+                        throw new Error(response);
+                    }
+                })
                 .catch(error => {
                     console.log('Registration failed:', error);
                 });
         } else {
             console.log('Invalid form');
-            
+
         }
     };
-    
+
     return (
         <div className="abc">
             <div className="main-continer">
-            <div className="heading1">
-                <p className="text1">
-                    Welcome Page
-                    <br /> One line text <br /> Will be here{" "}
-                </p>
-                <p className="text2">Sign in to continue access pages</p>
-                <p className="text3">Don’t Have An Account?</p>
-                
-                <Link to="/">
-                <button className="register">Sign In </button></Link>
-            </div>
-            <div className="Register">
-                <form onSubmit={handleSubmit}>
-                    <h1>Register </h1>
-                    <p className="text">Register in to continue access pages</p>
-                    {
-                    fail && <p>{fail}</p>
-                }
-                    <div className="rows">
-                        <div className="form-flex">
-                            <label className="label" htmlFor="name" >Name</label>
-                            <input type="text" id="name" className="input-field" value={name}
+                <div className="heading1">
+                    <p className="text1">
+                        Welcome Page
+                        <br /> One line text <br /> Will be here{" "}
+                    </p>
+                    <p className="text2">Sign in to continue access pages</p>
+                    <p className="text3">Don’t Have An Account?</p>
+
+                    <Link to="/">
+                        <button className="reg-btn">Sign In </button></Link>
+                </div>
+                <div className="Register">
+                    <form onSubmit={handleSubmit}>
+                        <h1>Register </h1>
+                        <p className="text">Register in to continue access pages</p>
+                        {
+                            fail && <p>{fail}</p>
+                        }
+                        <div className="rows">
+                            <div className="form-flex">
+                                <label className="label" htmlFor="name" >Name</label>
+                                <input type="text" id="name" className="input-field" value={name}
                                     onChange={(e) => setName(e.target.value)} required />
                                 {errors.name && <span>{errors.name}</span>}
-                        </div>
-                        <div className="form-flex">
-                            <label className="label" htmlFor="email">Email</label>
-                            <input type="email" id="email" className="input-field" value={email}
-                                onChange={(e) => setEmail(e.target.value)} required />{errors.email && <span>{errors.email}</span>}
-                        </div>
-                        <div className="form-flex">
-                            <label className="label" htmlFor="number">Phone number</label>
-                            <input type="number" id="number" className="input-field" value={phoneNumber}
+                            </div>
+                            <div className="form-flex">
+                                <label className="label" htmlFor="email">Email</label>
+                                <input type="email" id="email" className="input-field" value={email}
+                                    onChange={(e) => setEmail(e.target.value)} required />{errors.email && <span>{errors.email}</span>}
+                            </div>
+                            <div className="form-flex">
+                                <label className="label" htmlFor="number">Phone number</label>
+                                <input type="number" id="number" className="input-field" value={phoneNumber}
                                     onChange={(e) => setPhoneNumber(e.target.value)} required />
                                 {errors.phoneNumber && <span>{errors.phoneNumber}</span>}
-                        </div>
-                        <div className="form-flex">
-                            <label className="label" htmlFor="pro">Profession</label>
-                            <input type="Text" id="pro" className="input-field" value={profession}
+                            </div>
+                            <div className="form-flex">
+                                <label className="label" htmlFor="pro">Profession</label>
+                                <input type="Text" id="pro" className="input-field" value={profession}
                                     onChange={(e) => setProfession(e.target.value)} required />
                                 {errors.profession && <span>{errors.profession}</span>}
-                        </div>
-                        <div className="form-flex">
-                            <label className="label" htmlFor="pass">Password</label>
-                            <input type="password" id="pass" className="input-field" value={password}
-                                onChange={(e) => setPassword(e.target.value)} required />
-                            {errors.password && <span>{errors.password}</span>}
+                            </div>
+                            <div className="form-flex">
+                                <label className="label" htmlFor="pass">Password</label>
+                                <input type="password" id="pass" className="input-field" value={password}
+                                    onChange={(e) => setPassword(e.target.value)} required />
+                                {errors.password && <span>{errors.password}</span>}
 
-                        </div>
-                        <div className="form-flex">
-                            <label className="label" htmlFor="confirmPassword"> Confirm Password</label>
-                            <input type="password" id="confirmPassword" className="input-field" value={confirmPassword}
+                            </div>
+                            <div className="form-flex">
+                                <label className="label" htmlFor="confirmPassword"> Confirm Password</label>
+                                <input type="password" id="confirmPassword" className="input-field" value={confirmPassword}
                                     onChange={(e) => setConfirmPassword(e.target.value)} required />
                                 {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
+                            </div>
+                            <div className="checkbox">
+                                <label className="checkbox-label">
+                                    <input type="checkbox" className="checkbox-input" required />
+                                    I agree to Terms & Condition receiving marketing and promotional materials
+                                </label>
+                            </div>
                         </div>
-                        <div className="checkbox">
-                            <label className="checkbox-label">
-                                <input type="checkbox" className="checkbox-input" required/>
-                                I agree to Terms & Condition receiving marketing and promotional materials
-                            </label>
+                        <div className="btn1">
+                            <button className="reg-btn">Register</button>
+
                         </div>
-                    </div>
-                    <div className="btn1">
-                        <button>Register</button>
-                        
-                    </div>
-                </form>
-                
+                    </form>
+
+                </div>
             </div>
-        </div>
         </div>
     );
 }
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,119 +1,119 @@
 import React, { useState } from 'react'
-import { useNavigate ,} from 'react-router-dom';
+import { useNavigate, } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './SignIn.css'
-const REACT_APP_API_ENDPOINT='http://localhost:5001'
+const REACT_APP_API_ENDPOINT = 'http://localhost:5001'
 
 
 function SignIn() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState({});
-    const [fail, setFail] = useState('');
-    const navigate = useNavigate();
- 
-    const validateForm = () => {
-        const errors = {};
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+  const [fail, setFail] = useState('');
+  const navigate = useNavigate();
 
-        if (!email) {
-            errors.email = 'Email is required';
-        } else if (!/\S+@\S+\.\S+/.test(email)) {
-            errors.email = 'Invalid email address';
-        }
+  const validateForm = () => {
+    const errors = {};
 
-        if (!password) {
-            errors.password = 'Password is required';
-        }
-        console.log(errors);
-        setErrors(errors);
+    if (!email) {
+      errors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = 'Invalid email address';
+    }
 
-        return Object.keys(errors).length === 0;
-    };
+    if (!password) {
+      errors.password = 'Password is required';
+    }
+    console.log(errors);
+    setErrors(errors);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // console.log(validateForm());
-        if (validateForm()) {
-            const loginData = {
-                email,
-                password
-            };
-        fetch(`${REACT_APP_API_ENDPOINT}/login`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(loginData),
-          })
-            .then(response => {
-              if (response.status === 202) {
-                return response.json();
-              } else if (response.status === 401) {
-                throw new Error('Incorrect Password');
-              } else if (response.status === 404) {
-                throw new Error('No user Found Please Register First');
-              } else {
-                throw new Error('Login failed');
-              }
-            })
-            .then(data => {
-              const token = data.token;
-              // console.log('Login successful');
-              // console.log(token);
-              localStorage.setItem('token' , token)
-              localStorage.setItem('email' , email)
-              setFail('');
-              navigate('/surveyItems');
-            })
-            .catch(error => {
-              // console.log('Login failed:', error);
-              setFail(error.message);
-            });
-        } else {
-            // console.log('Invalid form');
-            
-        }
-    };
-    return (
-        <div className="abc">
-            <div className='main-continer'>
-            <div className='heading1'>
-                <p className='text1'>Welcome Page<br /> One line text <br /> Will be here </p>
-                <p className='text2'>Sign in to continue access pages</p>
-                <p className='text3'>Don’t Have An Account?</p>
-                
-                <Link to="/register">
-                <button className='register'>Register  </button></Link>
-            </div>
-            <div className='signin'>
-                <form>
-                    <h1>Sign In </h1>
-                    <p className='text'>Sign in to continue access pages</p> {
-                    fail && <p className='text'>{fail}</p>
-                }
-                    <div className="form-group">
-                        {/* <label className="label" htmlFor="email">Email</label>
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(validateForm());
+    if (validateForm()) {
+      const loginData = {
+        email,
+        password
+      };
+      fetch(`${REACT_APP_API_ENDPOINT}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+      })
+        .then(response => {
+          if (response.status === 202) {
+            return response.json();
+          } else if (response.status === 401) {
+            throw new Error('Incorrect Password');
+          } else if (response.status === 404) {
+            throw new Error('No user Found Please Register First');
+          } else {
+            throw new Error('Login failed');
+          }
+        })
+        .then(data => {
+          const token = data.token;
+          // console.log('Login successful');
+          // console.log(token);
+          localStorage.setItem('token', token)
+          localStorage.setItem('email', email)
+          setFail('');
+          navigate('/surveyItems');
+        })
+        .catch(error => {
+          // console.log('Login failed:', error);
+          setFail(error.message);
+        });
+    } else {
+      // console.log('Invalid form');
+
+    }
+  };
+  return (
+    <div className="abc">
+      <div className='main-continer'>
+        <div className='heading1'>
+          <p className='text1'>Welcome Page<br /> One line text <br /> Will be here </p>
+          <p className='text2'>Sign in to continue access pages</p>
+          <p className='text3'>Don’t Have An Account?</p>
+
+          <Link to="/register">
+            <button className='reg-btn'>Register  </button></Link>
+        </div>
+        <div className='signin'>
+          <form>
+            <h1>Sign In </h1>
+            <p className='text'>Sign in to continue access pages</p> {
+              fail && <p className='text'>{fail}</p>
+            }
+            <div className="form-group">
+              {/* <label className="label" htmlFor="email">Email</label>
                         <input type="email" id="email" className="input-field" required /> */}
-                          <label className="label" htmlFor="email">Email</label>
-                            <input type="email" id="email" className="input-field" value={email}
-                                onChange={(e) => setEmail(e.target.value)} required />{errors.email && <span>{errors.email}</span>}
-                    </div>
-                    <div className='form-group'>
-                        {/* <label htmlFor='Password'>Password</label>
-                        <input type='password' name='Password' required></input> */}
-                        <label className="label" htmlFor="pass">Password</label>
-                            <input type="password" id="pass" className="input-field" value={password}
-                                onChange={(e) => setPassword(e.target.value)} required />
-                            {errors.password && <span>{errors.password}</span>}
-                    </div>
-                    <div className='btn'>
-                        <button onClick={handleSubmit}>Sign in</button>
-                    </div>
-                </form>
+              <label className="label" htmlFor="email">Email</label>
+              <input type="email" id="email" className="input-field" value={email}
+                onChange={(e) => setEmail(e.target.value)} required />{errors.email && <span>{errors.email}</span>}
             </div>
+            <div className='form-group'>
+              {/* <label htmlFor='Password'>Password</label>
+                        <input type='password' name='Password' required></input> */}
+              <label className="label" htmlFor="pass">Password</label>
+              <input type="password" id="pass" className="input-field" value={password}
+                onChange={(e) => setPassword(e.target.value)} required />
+              {errors.password && <span>{errors.password}</span>}
+            </div>
+            <div className='btn'>
+              <button className='reg-btn' onClick={handleSubmit}>Sign in</button>
+            </div>
+          </form>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default SignIn
